6754b99bec01f0845f5b189241d783f9ff28623d
rohan <rohanverma031@gmail.com>
2025-01-17 14:53:32 +0530

    feat: added hub to manage all the connected clients

--- hub/hub.go
+++ hub/hub.go
@@ -1,9 +1,12 @@
 package hub
 
 import (
+	"doki.co.in/doki_real_time_service/client"
+	"doki.co.in/doki_real_time_service/helper"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
 )
 
 // Hub handles all the client connection and related methods
@@ -14,6 +17,62 @@ var websocketUpgrader = websocket.Upgrader{
 }
 
 type Hub struct {
+	sync.RWMutex
+	clients client.ClientList
+}
+
+// AddClient adds newly connected client to Hub
+func (h *Hub) AddClient(user string, client *client.Client) {
+	h.Lock()
+	defer h.Unlock()
+
+	username, resource := helper.GetUsernameAndResourceFromUser(user)
+	if username == "" || resource == "" {
+		return
+	}
+
+	h.clients[username][resource] = client
+}
+
+// RemoveClient closes and removes connection from Hub
+func (h *Hub) RemoveClient(user string) {
+	h.Lock()
+	defer h.Unlock()
+
+	username, resource := helper.GetUsernameAndResourceFromUser(user)
+	if username == "" || resource == "" {
+		return
+	}
+
+	if conn, ok := h.clients[username][resource]; ok {
+		// close the websocket connection
+		_ = conn.Connection.Close()
+
+		// remove resource from username
+		delete(h.clients[username], resource)
+
+		// if empty remove the username too
+		if len(h.clients[username]) == 0 {
+			delete(h.clients, username)
+		}
+	}
+}
+
+// GetIndividualClient is used to get user connected client in particular resource
+// this will be used when server needs to send updates for the post and other user subscriptions
+func (h *Hub) GetIndividualClient(user string) *client.Client {
+	username, resource := helper.GetUsernameAndResourceFromUser(user)
+	if username == "" || resource == "" {
+		return nil
+	}
+
+	return h.clients[username][resource]
+}
+
+// GetAllConnectedClients will return all the connected clients for a particular user
+// this will be used when forwarding user messages
+func (h *Hub) GetAllConnectedClients(username string) map[string]*client.Client {
+	return h.clients[username]
 }
 
 // ServeWS methods takes the current [http] request
