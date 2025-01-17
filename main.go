aa2ea6794781d0efb2ec3df43e8470fdedcbbef1
rohan <rohanverma031@gmail.com>
2025-01-17 16:17:53 +0530

    feat: added websocket endpoint

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"doki.co.in/doki_real_time_service/hub"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+
 	fmt.Println("Doki real time service")
+
+	newHub := hub.CreateHub()
+	http.HandleFunc("/ws", newHub.ServeWS)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
