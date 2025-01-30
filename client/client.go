37602c4c64f2cbd1ed72a94611a0ad063a36d927
rohan <rohanverma031@gmail.com>
2025-01-30 10:47:28 +0530

    feat: created client pacakge with global client interface

---
+++ client/client.go
@@ -0,0 +1,10 @@
+package client
+
+import "github.com/gorilla/websocket"
+
+// Client is the global interface used by payload and hub to store and get clients
+type Client interface {
+	GetConnection() *websocket.Conn
+	GetUserInfo() (string, string)
+	WriteToChannel(*[]byte)
+}
